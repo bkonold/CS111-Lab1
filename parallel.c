9ecae2693312ffe4807a60fc9602301b385f5e48
bkonold <brettkonold@gmail.com>
2014-04-23 20:16:39 -0700

    Fixed the fork bomb bug
    If fork fails, wait. Also, maintain an "available processes" counter.
    When it hits zero, wait for processes to finish. There is still a bug
    where the "available processes" variable does not restrict the number of
    processes that are actually created (this is only happening on my
    machine, it works fine on SEAS). Seems to be that numAvailableProcesses
    is never hitting zero so never triggers a wait....

--- parallel.c
+++ parallel.c
@@ -4,167 +4,200 @@
 #include "parallel.h"
 #include "stack.h"
 #include <errno.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define MAX_PROCS 30
 
 static bool* sharedFinished;
 static int* numFreeProcesses;
+static pthread_mutex_t* mutp;
 
 void
 build_io_lists(command_t cmd, string_list_t readList, string_list_t writeList) {
-	if (cmd->type == SIMPLE_COMMAND) {
-		int i = 1;
-		if (strcmp(cmd->u.word[0], "exec") == 0)
-			i = 2;
+    if (cmd->type == SIMPLE_COMMAND) {
+        int i = 1;
+        if (strcmp(cmd->u.word[0], "exec") == 0)
+            i = 2;
 
-		while (cmd->u.word[i]) {
-			if (cmd->u.word[i][0] != '-') {
-				push_back(readList, cmd->u.word[i]);
-			}
-			i++;
-		}
-		if (cmd->output)
-			push_back(writeList, cmd->output);
-		if (cmd->input)
-			push_back(readList, cmd->input);
-	}
-	else if (cmd->type == SUBSHELL_COMMAND) {
-		if (cmd->input)
-			push_back(readList, cmd->input);
-		if (cmd->output)
-			push_back(writeList, cmd->output);
-		build_io_lists(cmd->u.subshell_command, readList, writeList);
-	}
-	else {
-		build_io_lists(cmd->u.command[0], readList, writeList);
-		build_io_lists(cmd->u.command[1], readList, writeList);
-	}
+        while (cmd->u.word[i]) {
+            if (cmd->u.word[i][0] != '-') {
+                push_back(readList, cmd->u.word[i]);
+            }
+            i++;
+        }
+        if (cmd->output)
+            push_back(writeList, cmd->output);
+        if (cmd->input)
+            push_back(readList, cmd->input);
+    }
+    else if (cmd->type == SUBSHELL_COMMAND) {
+        if (cmd->input)
+            push_back(readList, cmd->input);
+        if (cmd->output)
+            push_back(writeList, cmd->output);
+        build_io_lists(cmd->u.subshell_command, readList, writeList);
+    }
+    else {
+        build_io_lists(cmd->u.command[0], readList, writeList);
+        build_io_lists(cmd->u.command[1], readList, writeList);
+    }
 }
 
 bool
 intersect(string_list_t first, string_list_t second) {
-	if (empty(first) || empty(second))
-		return false;
+    if (empty(first) || empty(second))
+        return false;
 
-	node_t currReadNode = first->head;
-	while (currReadNode) {
+    node_t currReadNode = first->head;
+    while (currReadNode) {
 
-		char* currReadWord = currReadNode->item;
-		node_t currWriteNode = second->head;
+        char* currReadWord = currReadNode->item;
+        node_t currWriteNode = second->head;
 
-		while (currWriteNode) {
-			char* currWriteWord = currWriteNode->item;
+        while (currWriteNode) {
+            char* currWriteWord = currWriteNode->item;
 
-			if (strcmp(currReadWord, currWriteWord) == 0)
-				return true;
+            if (strcmp(currReadWord, currWriteWord) == 0)
+                return true;
 
-			currWriteNode = currWriteNode->next;
-		}
+            currWriteNode = currWriteNode->next;
+        }
 
-		currReadNode = currReadNode->next;
-	}
+        currReadNode = currReadNode->next;
+    }
 
-	return false;
+    return false;
 }
 
 void
 add_to_graph(graphnode_list_t adjList, command_t cmd, string_list_t readList, string_list_t writeList) {
-	graphnode_t newNode = checked_malloc(sizeof(struct graphnode));
-	newNode->cmd = cmd;
-	newNode->readList = readList;
-	newNode->writeList = writeList;
-	newNode->aid = -1;
-	newNode->dependencies = create_list();
+    graphnode_t newNode = checked_malloc(sizeof(struct graphnode));
+    newNode->cmd = cmd;
+    newNode->readList = readList;
+    newNode->writeList = writeList;
+    newNode->aid = -1;
+    newNode->dependencies = create_list();
 
-	node_t currNode = adjList->head;
-	while (currNode) {
-		graphnode_t currGraphNode = currNode->item;
+    node_t currNode = adjList->head;
+    while (currNode) {
+        graphnode_t currGraphNode = currNode->item;
 
-		if (intersect(newNode->readList, currGraphNode->writeList)     /* read after write  */
-			|| intersect(newNode->writeList, currGraphNode->writeList) /* write after write */
-			|| intersect(newNode->writeList, currGraphNode->readList)  /* write after read  */) {
+        if (intersect(newNode->readList, currGraphNode->writeList)     /* read after write  */
+            || intersect(newNode->writeList, currGraphNode->writeList) /* write after write */
+            || intersect(newNode->writeList, currGraphNode->readList)  /* write after read  */) {
 
-			push_back(newNode->dependencies, currGraphNode);
-		}
+            push_back(newNode->dependencies, currGraphNode);
+        }
 
-		currNode = currNode->next;
-	}
+        currNode = currNode->next;
+    }
 
-	push_back(adjList, newNode);
+    push_back(adjList, newNode);
 
 }
 
 void execute_parallel(command_stream_t commandStream) {
-	graphnode_list_t adjList = create_list();
-	command_t cmd;
-	while ((cmd = read_command_stream (commandStream))) {
-		string_list_t readList = create_list(); 
-		string_list_t writeList = create_list();
-		build_io_lists(cmd, readList, writeList);
-		add_to_graph(adjList, cmd, readList, writeList);
-	}
+    graphnode_list_t adjList = create_list();
+    command_t cmd;
+    while ((cmd = read_command_stream (commandStream))) {
+        string_list_t readList = create_list(); 
+        string_list_t writeList = create_list();
+        build_io_lists(cmd, readList, writeList);
+        add_to_graph(adjList, cmd, readList, writeList);
+    }
 
-	int adjListSize = size(adjList);
+    int adjListSize = size(adjList);
 
-	sharedFinished = mmap(NULL, adjListSize * sizeof(bool), 
-				    PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+    sharedFinished = mmap(NULL, adjListSize * sizeof(bool), 
+                    PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
 
-	numFreeProcesses = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+    numFreeProcesses = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
 
-	*numFreeProcesses = MAX_PROCS;
+    *numFreeProcesses = MAX_PROCS;
 
-	int i;
-	for (i = 0; i < adjListSize; i++) {
-		sharedFinished[i] = false;
-	}
+    mutp = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
 
-	i = 0;
-	node_t currNode = adjList->head;
-	while (currNode) {
-		graphnode_t currGraphNode = currNode->item;
-		currGraphNode->aid = i;
+    pthread_mutexattr_t attr;
+    pthread_mutexattr_init (&attr);
+    pthread_mutexattr_setpshared (&attr, PTHREAD_PROCESS_SHARED);
 
-		while (*numFreeProcesses < 1)
-			continue;
+    pthread_mutex_init (mutp, &attr);
+    pthread_mutexattr_destroy (&attr);
 
-		pid_t p = fork();
+    int i;
+    for (i = 0; i < adjListSize; i++) {
+        sharedFinished[i] = false;
+    }
 
-		if (p == 0) {
-			(*numFreeProcesses)--;
-			execute_node(currGraphNode);
-		}
-		else if (p > 0) {
-			currNode = currNode->next;
-		}
-		i++;
-	}
-	while (true) {
-		int status;
-		pid_t done = wait(&status);
-		if (done == -1 && errno == ECHILD) {
-			break;
-		}
-	}
+    i = 0;
+    node_t currNode = adjList->head;
+    while (currNode) {
+        graphnode_t currGraphNode = currNode->item;
+        currGraphNode->aid = i;
+
+        if (*numFreeProcesses == 0) {
+            //printf("\n*****\n\n*****\n\n*****\n\nfucked\n\n****\n\n****\n\n****\n");
+        }
+
+        while (*numFreeProcesses < 1) {
+            // wait for a child to finish beore forking again
+            //printf("waiting for a child to finish\n");
+            wait(NULL);
+        }
+
+        pid_t p = fork();
+
+        if (p == 0) {
+            execute_node(currGraphNode);
+        }
+        else if (p > 0) {
+            pthread_mutex_lock (mutp);
+           // printf("\nparent aquired lock\n");
+            (*numFreeProcesses)--;
+            //printf("(parent)numFreeProcesses is %d\n", *numFreeProcesses);
+           // printf("\nparent released lock\n");
+            pthread_mutex_unlock (mutp);
+            currNode = currNode->next;
+        }
+        else if (p == -1) {
+            // forked too many times, need to wait!
+            wait(NULL);
+        }
+        i++;
+    }
+    while (true) {
+        int status;
+        pid_t done = wait(&status);
+        if (done == -1 && errno == ECHILD) {
+            break;
+        }
+    }
 }
 
 void
 execute_node(graphnode_t node) {
-	// wait for dependencies to finish
-	node_t currNode = node->dependencies->head;
-	while (currNode) {
-		graphnode_t currDependencyNode = currNode->item;
+    // wait for dependencies to finish
+    node_t currNode = node->dependencies->head;
+    while (currNode) {
+        graphnode_t currDependencyNode = currNode->item;
 
-		while(!sharedFinished[currDependencyNode->aid])
-			continue;
+        while(!sharedFinished[currDependencyNode->aid])
+            continue;
 
-		currNode = currNode->next;
-	}
-	execute_command(node->cmd);
-	sharedFinished[node->aid] = true;
-	(*numFreeProcesses)++;
-	exit(node->cmd->status);
+        currNode = currNode->next;
+    }
+    execute_command(node->cmd);
+    sharedFinished[node->aid] = true;
+    pthread_mutex_lock (mutp);
+    //printf("\nchild aquired lock\n");
+    (*numFreeProcesses)++;
+    //printf("(child)numFreeProcesses is %d\n", *numFreeProcesses);
+    //printf("child released lock\n\n");
+    pthread_mutex_unlock (mutp);
+    exit(node->cmd->status);
 }
